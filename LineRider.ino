c4285907e91c0b4dec2e87694fff574d69f86121
Justin Hartmann <zadarimm@gmail.com>
2020-03-16 05:50:04 -0400

    changes

--- LineRider.ino
+++ LineRider.ino
@@ -1,44 +1,44 @@
 #include <Servo.h>
-#include <PID_v1.h>
 #include <A4990MotorShield.h>
 #include <QTRSensors.h>
 
 /* constants */
 
 // motor speed offsets (to account for any slight difference in speed when going at max speed)
-#define LEFT_MOTOR_STATIC_OFFSET      0
-#define RIGHT_MOTOR_STATIC_OFFSET     0
+#define LEFT_MOTOR_STATIC_OFFSET        0
+#define RIGHT_MOTOR_STATIC_OFFSET       0
 
-// pid constants
-#define Kp                            0
-#define Ki                            0
-#define Kd                            0
+#define FLIP_L                          1
+#define FLIP_R                          1
 
-// motor speed constants (ADJUST AS NEEDED)
-#define GOAL_SPEED                   200
-#define MAX_SPEED                   400
+#define SETPOINT                     2500//3500
 
+// pid constants (200 max speed)
 
-#define SENSOR_COUNT                  8 // # of sensors on sensor board
-#define CALLIBRATE_TIME           10000 // target time length of callibration of sensors
+#define GOAL_SPEED                     75
+#define MAX_SPEED                     125
+
+#define Kp                        0.100000
+#define Ki                        0.000000
+#define Kd                        0.000000
 
 // motor speed offsets and multipliers (to account for any slight difference in speed when going at max speed or wiring flips)
 
-#define SENSOR_COUNT     8     // number of sensors used
+#define SENSOR_COUNT                  6     // number of sensors used
 
-#define EMITTER_PIN      2 // sensor emitter control pin
+#define EMITTER_PIN                   2 // sensor emitter control pin
 
 // sensor input pins
-#define SENSOR_PIN_1    A0
-#define SENSOR_PIN_2    A1
-#define SENSOR_PIN_3    A2
-#define SENSOR_PIN_4    A3
-#define SENSOR_PIN_5    A4
-#define SENSOR_PIN_6    A5
-#define SENSOR_PIN_7    A6
-#define SENSOR_PIN_8    A7
+#define SENSOR_PIN_1                 A0
+#define SENSOR_PIN_2                 A1
+#define SENSOR_PIN_3                 A2
+#define SENSOR_PIN_4                 A3
+#define SENSOR_PIN_5                 A4
+#define SENSOR_PIN_6                 A5
+#define SENSOR_PIN_7                  4
+#define SENSOR_PIN_8                  5
 
-#define FAN_CONTROL_PIN  3 // ducted fan speed control pin (pwm)
+#define FAN_CONTROL_PIN               3 // ducted fan speed control pin (pwm)
 
 // constant array for storing sensor pins for constructor of sensor library
 const uint8_t sensorPins[SENSOR_COUNT] = { 
@@ -47,9 +47,9 @@ const uint8_t sensorPins[SENSOR_COUNT] = {
                                            SENSOR_PIN_3,
                                            SENSOR_PIN_4,
                                            SENSOR_PIN_5,
-                                           SENSOR_PIN_6,
-                                           SENSOR_PIN_7,
-                                           SENSOR_PIN_8 
+                                           SENSOR_PIN_6
+//                                           SENSOR_PIN_7,
+//                                           SENSOR_PIN_8 
                                          };
 
 // array for storing input values from sensor
@@ -60,12 +60,7 @@ QTRSensors sensor;
 // the library for controlling the motors
 A4990MotorShield motors;
 
-double setpoint;
-double input;
-double output;
-
-// the library for the pid controller       P   I   D
-PID controller(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+int lastError;
 
 // the ducted fan control is the same as a servo's, se we use the servo library
 Servo fan;
@@ -74,17 +69,13 @@ void setup() {
   Serial.begin(9600);
   
   setupPinModes();
-  
-  sensor.setTypeAnalog();
-  sensor.setSensorPins(sensorPins, SENSOR_COUNT);
-  sensor.setEmitterPin(EMITTER_PIN);
 
   doCallibration();
   
   motors.setSpeeds(0, 0);
   delay(2000); // wait two seconds for human to center manually
 
-  setFan(180);
+//  setFan(180);
 }
 
 void setupPinModes() {
@@ -100,14 +91,28 @@ void setupPinModes() {
 
 void doCallibration() {
   digitalWrite(LED_BUILTIN, HIGH);
-  callibrateFan();
-  callibrateSensors();
+  setupSensor();
+//  callibrateFan();
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void callibrateFan() {
-  Serial.println("Callibrating fan...");
+void setupSensor() {
+  sensor.setTypeRC();
+  sensor.setSensorPins(sensorPins, SENSOR_COUNT);
+  sensor.setEmitterPin(EMITTER_PIN);
   
+  Serial.println("Callibrating sensors...");
+
+  for (uint16_t i = 0; i < 200; i++) {
+    sensor.calibrate();
+  }
+  motors.setSpeeds(0, 0);
+  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+  delay(1000);
+}
+
+void callibrateFan() {
   fan.attach(FAN_CONTROL_PIN);
 
   Serial.println("Callibrating fan...");
@@ -128,47 +133,86 @@ void callibrateFan() {
   fan.write(0); // let fan rest for further callibration
 }
 
-void callibrateSensors() {
-  Serial.println("Callibrating sensors...");
-
-  int totalI = 100; // CALLIBRATE_TIME / 40;
-  
-  for(int i = 0; i < totalI; i++) {
-    if(i < totalI * 0.25 || i >= totalI * 0.75) {
-     turn(1);
-    } else {
-     turn(-1);
-    }
-    sensor.calibrate();
-  }
-  delay(20);
-}
-
-void turn(const int amount) {
-  motors.setSpeeds(amount / 2, -(amount / 2));
-  delayMicroseconds(20000);
-}
-
-void loop() {
+ void loop() {
   // 3500 is offset for line sensor input
-  input = sensor.readLineBlack(sensorValues) - 3500;
+  int error = sensor.readLineBlack(sensorValues) - SETPOINT;
 
-  controller.Compute();
+  double output = (Kp * (double)error) + (Kd * ((double)error - (double)lastError));
 
-  setMotorsDiff(GOAL_SPEED, output);
+  lastError = error;
+
+//  Serial.println(output);
+  
+//  setMotorsDiff(vel, output);
+//  setMotorsDiff(GOAL_SPEED, output);
+  setMotorsD(GOAL_SPEED, output);
 }
 
-void setMotorsDiff(const int goalSpeed, const int diff) {
-  int rightSpeed = goalSpeed + diff;
-  int leftSpeed = goalSpeed - diff;
+void setMotorsD(const int goalSpeed, int diff) {
+  int leftSpeed = goalSpeed;
+  int rightSpeed = goalSpeed;
+  
+  if(diff < 0) {
+    rightSpeed = goalSpeed + abs(diff);
+  } else if(diff > 0) {
+    leftSpeed = goalSpeed + abs(diff);
+  }
 
-  if(rightSpeed > MAX_SPEED) rightSpeed = MAX_SPEED;
-  if(leftSpeed > MAX_SPEED) leftSpeed = MAX_SPEED;
+  if(rightSpeed > MAX_SPEED) {
+    leftSpeed -= (MAX_SPEED - rightSpeed);
+    rightSpeed = MAX_SPEED;
+  }
+  if(leftSpeed > MAX_SPEED) {
+    rightSpeed -= (MAX_SPEED - leftSpeed);
+    leftSpeed = MAX_SPEED;
+  }
+  
+//  if(rightSpeed < 0){
+//    leftSpeed += rightSpeed;
+//    if(leftSpeed > MAX_SPEED) leftSpeed = MAX_SPEED;
+//    rightSpeed = 0;
+//  }
+//  if(leftSpeed < 0){
+//    rightSpeed += leftSpeed;
+//    if(rightSpeed > MAX_SPEED) rightSpeed = MAX_SPEED;
+//    leftSpeed = 0;
+//  }
+
+//  motors.setSpeeds(rightSpeed * FLIP_R, leftSpeed * FLIP_L); // might need to switch these; do tests to confirm placement
+  motors.setSpeeds(leftSpeed * FLIP_L ,rightSpeed * FLIP_R); // might need to switch these; do tests to confirm placement
+}
+
+void setMotorsDiff(const int goalSpeed, int diff) {
+  diff *= 2.0;
+  
+  int rightSpeed = goalSpeed;// - (diff / 2.0);
+  int leftSpeed = goalSpeed + (diff / 2.0);
+  
+//  if(diff > 0) {
+//    rightSpeed = GOAL_SPEED;
+//    leftSpeed = rightSpeed - diff;
+//  } else if(diff < 0) {
+//    leftSpeed = GOAL_SPEED;
+//    rightSpeed = leftSpeed - diff;
+//  } else {
+//    leftSpeed = GOAL_SPEED;
+//    rightSpeed = GOAL_SPEED;
+//  }
+
+  if(rightSpeed > MAX_SPEED) {
+    leftSpeed -= (MAX_SPEED - rightSpeed);
+    rightSpeed = MAX_SPEED;
+  }
+  if(leftSpeed > MAX_SPEED) {
+    rightSpeed -= (MAX_SPEED - leftSpeed);
+    leftSpeed = MAX_SPEED;
+  }
   
   if(rightSpeed < 0) rightSpeed = 0;
   if(leftSpeed < 0) leftSpeed = 0;
 
-  motors.setSpeeds(rightSpeed, leftSpeed); // might need to switch these; do tests to confirm placement
+//  motors.setSpeeds(rightSpeed * FLIP_R, leftSpeed * FLIP_L); // might need to switch these; do tests to confirm placement
+  motors.setSpeeds(leftSpeed * FLIP_L ,rightSpeed * FLIP_R); // might need to switch these; do tests to confirm placement
 }
 
 void setFan(const int fanSpeed) {
